ed70e9ffaec1d04d989894f47bc91d532ba03838
Sastha <shazcool@gmail.com>
2018-02-15 19:17:11 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+      <vue-time></vue-time>
+  </div>
+</template>
+
+<script>
+import VueTime from './components/VueTime'
+
+export default {
+  name: 'app',
+  components: {
+    VueTime
+  }
+}
+</script>
